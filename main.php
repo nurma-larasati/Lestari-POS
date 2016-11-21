114aabd1bf4b03b93321b1ed71514504cdbabce2
nurma-larasati <nurmalarasati@gmail.com>
2016-11-21 10:58:29 +0000

    The first commit.

---
+++ main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Points of Sale</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+<table>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+</table>
+</body>
+
+</html>
